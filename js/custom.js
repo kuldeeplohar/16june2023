f11cacd7aaa593d7ad5269b7588bfd3f9bee8339
kuldeeplohar <loharkuldeep169@gmail.com>
2023-07-07 17:35:46 +0530

    js

---
+++ js/custom.js
@@ -0,0 +1,270 @@
+
+    //Define
+function getMyFunction(){
+    console.log("A");
+}
+    //Calling
+   getMyFunction();
+
+//fat arrow
+  //define
+var bFunction=()=>{
+    console.log('b')
+}
+  //callin/invoking
+  bFunction();
+
+  //old method
+
+  //define
+function cFunction(){
+  console.log(`C`);
+}
+  //calling
+  cFunction();
+
+  //fat arrow
+  //define
+var dFunction=()=>{
+    console.log('d')
+}
+  //callin/invoking
+  dFunction();
+
+    //old method
+
+  //define
+function eFunction(){
+    console.log(`e`);
+  }
+    //calling
+    eFunction();
+  
+    //fat arrow
+    //define
+  var fFunction=()=>{
+      console.log('f')
+  }
+    //callin/invoking
+    fFunction();
+
+    //old method
+    //define\
+
+    function gFunction(){
+        console.log('G');
+    }
+    //cALLING
+
+    gFunction();
+
+    //FAT ARROW
+
+    //define
+
+    let hFunction=()=>{
+        console.log('h');
+    }
+    hFunction(); //call
+
+    //old method
+    //define
+
+    function iFunction(){
+        console.log(`i`);
+    }
+
+    iFunction();
+
+    //Fat arrow
+
+    //define
+
+    const jFunction=()=>{
+        console.log('j');
+    }
+
+    //calling
+    jFunction();
+
+    //old method
+    //define
+
+    function kFunction(){
+        console.log("k")
+    }
+
+    //calling
+      
+      kFunction();
+
+    //fat arrow 
+    //define
+
+    var lFunction=()=>{
+        console.log("l")
+    }
+
+    //call
+
+    lFunction();
+
+
+       //old method
+    //define
+
+    function mFunction(){
+        console.log("m")
+    }
+
+    //calling
+      
+      mFunction();
+
+    //fat arrow 
+    //define
+
+    var nFunction=()=>{
+        console.log("n")
+    }
+
+    //call
+
+    nFunction();
+
+
+       //old method
+    //define
+
+    function oFunction(){
+        console.log("o")
+    }
+
+    //calling
+      
+      oFunction();
+
+    //fat arrow 
+    //define
+
+    var pFunction=()=>{
+        console.log("p")
+    }
+
+    //call
+
+    pFunction();
+
+       //old method
+    //define
+
+    function qFunction(){
+        console.log("q")
+    }
+
+    //calling
+      
+      qFunction();
+
+    //fat arrow 
+    //define
+
+    var rFunction=()=>{
+        console.log("r")
+    }
+
+    //call
+
+    rFunction();
+
+
+       //old method
+    //define
+
+    function sFunction(){
+        console.log("s")
+    }
+
+    //calling
+      
+      sFunction();
+
+    //fat arrow 
+    //define
+
+    var tFunction=()=>{
+        console.log("t")
+    }
+
+    //call
+
+    tFunction();
+
+       //old method
+    //define
+
+    function uFunction(){
+        console.log("u")
+    }
+
+    //calling
+      
+      uFunction();
+
+    //fat arrow 
+    //define
+
+    var vFunction=()=>{
+        console.log("v")
+    }
+
+    //call
+
+    vFunction();
+
+       //old method
+    //define
+
+    function wFunction(){
+        console.log("w")
+    }
+
+    //calling
+      
+      wFunction();
+
+    //fat arrow 
+    //define
+
+    var xFunction=()=>{
+        console.log("x")
+    }
+
+    //call
+
+    xFunction();
+
+       //old method
+    //define
+
+    function yFunction(){
+        console.log("y")
+    }
+
+    //calling
+      
+      yFunction();
+
+    //fat arrow 
+    //define
+
+    var zFunction=()=>{
+        console.log("z")
+    }
+
+    //call
+
+    zFunction();
+
+
+
